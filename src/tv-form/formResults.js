2c8074def8702441c54697e61584090a00aa8b13
Adem Huduti <30794812+AdemHuduti@users.noreply.github.com>
2018-07-02 11:19:13 +0200

    Update formResults.js

--- src/tv-form/formResults.js
+++ src/tv-form/formResults.js
@@ -29,34 +29,3 @@ const FormResults = props => {
 }
 
 export default FormResults;
-
-
-// export class FormResults extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   render() {
-//     const link = 'https://image.tmdb.org/t/p/w300';
-//     return (
-//       <ul id="results" >
-//         {
-//           this.props.results.map((element, index) => {
-//             return (
-//               <li key={index} onClick={this.handleClick}>
-//                 <Link to={`/tvshow/${this.props.results[index].id}`} >
-//                   <img src={this.props.results[index].poster_path === null ? 'http://via.placeholder.com/300x450' : `${link}${this.props.results[index].poster_path}`} alt={`${this.props.results[index].title} poster`} className="resultPoster" />
-//                   <div>
-//                     <p>{this.props.results[index].name}</p>
-//                     <p>{this.props.results[index].first_air_date}</p>
-//                   </div>
-//                 </Link>
-//               </li>
-//             )
-//           })
-//         }
-//       </ul>
-//     );
-//   }
-// }
